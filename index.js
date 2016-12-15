b194eaa969a3ff89a6354b70bb6ae79132c7ed4d
Jared Hanson <jaredhanson@gmail.com>
2016-12-14 17:35:09 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+exports = module.exports = {
+  'client': require('./xom/client'),
+  'mfaclient': require('./xom/mfaclient'),
+  'challenge': require('./xom/challenge'),
+  'makecredential': require('./xom/makecredential'),
+  'ds/credentials': require('./xom/ds/credentials'),
+  'otp/verify': require('./xom/otp/verify')
+};
+
+exports.load = function(id) {
+  try {
+    return require('./xom/' + id);
+  } catch (ex) {
+    if (ex.code == 'MODULE_NOT_FOUND') { return; }
+    throw ex;
+  }
+};
