fad5caf10fd865114a33c8fc8e15dc7ee5bfb6b6
Jared Hanson <jaredhanson@gmail.com>
2016-12-14 17:39:50 -0800

    Add credentials directory.

---
+++ xom/ds/credentials.js
@@ -0,0 +1,19 @@
+exports = module.exports = function(idmap, client) {
+  // Load modules.
+  var Directory = require('../../lib/creds/directory');
+  
+  var directory = new Directory(client, idmap);
+  return directory;
+};
+
+// TODO: Rename this to pip/users/credentials
+
+exports['@implements'] = [
+  'http://schemas.authnomicon.org/js/login/mfa/CredentialDirectory',
+  'http://schemas.authnomicon.org/js/login/mfa/opt/auth0/CredentialDirectory'
+];
+exports['@singleton'] = true;
+exports['@require'] = [
+  '../id/map',
+  'http://schemas.authnomicon.org/js/opt/auth0/mgmt/Client'
+];
