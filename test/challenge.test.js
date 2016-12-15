9f0d6db3ede41852f450779f1b9115d9565b077d
Jared Hanson <jaredhanson@gmail.com>
2016-12-15 10:39:32 -0800

    Add OTP challenge.

--- test/challenge.test.js
+++ test/challenge.test.js
@@ -23,9 +23,9 @@ describe('auth0/challenge', function() {
       sendPush: function(){}
     };
     var idmap;
+    
   
-  
-    describe('via out-of-band authenticator type', function() {
+    describe('an out-of-band authenticator', function() {
       var params;
       
       before(function() {
@@ -70,7 +70,46 @@ describe('auth0/challenge', function() {
         expect(params.type).to.equal('oob');
         expect(params.txid).to.equal('0a0zz000-aaaa-0aa0-a000-00a0aaa00a0a');
       });
-    }); // via out-of-band authenticator type
+    }); // an out-of-band authenticator
+    
+    describe('an OTP authenticator', function() {
+      var params;
+      
+      before(function() {
+        // TODO: What does this look like?
+        var result = {
+        };
+        
+        sinon.stub(client, 'sendPush').yields(null, result);
+        idmap = sinon.stub().yields(null, 'auth0|00xx00x0000x00x0000x0000');
+      });
+    
+      after(function() {
+        client.sendPush.restore();
+      });
+      
+      before(function(done) {
+        var challenge = factory(idmap, client);
+        challenge({ id: '1', username: 'johndoe' }, 'dev_xxxXxxX0XXXxXx0X', { type: 'otp' }, function(_err, _params) {
+          if (_err) { return done(_err); }
+          params = _params;
+          done();
+        });
+      });
+    
+      it('should not map user identifier', function() {
+        expect(idmap.callCount).to.equal(0);
+      });
+    
+      it('should not send push notification', function() {
+        expect(client.sendPush.callCount).to.equal(0);
+      });
+      
+      it.skip('should yield parameters', function() {
+        expect(params.type).to.equal('oob');
+        expect(params.txid).to.equal('0a0zz000-aaaa-0aa0-a000-00a0aaa00a0a');
+      });
+    }); // an OTP authenticator
     
   }); // challenge
   
