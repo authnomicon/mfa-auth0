4dbb290f466ac5acbb47cd44522a4e425c3130ec
Jared Hanson <jaredhanson@gmail.com>
2016-12-16 12:36:45 -0800

    Add test case for already enrolled with SMS.

--- test/client/browserish.test.js
+++ test/client/browserish.test.js
@@ -260,6 +260,88 @@ describe('auth0/client/browserish', function() {
         });
       }); // already enrolled with Auth0 Guardian
       
+      describe('already enrolled with SMS', function() {
+        var guardian;
+        var mfaClient = {
+          httpClient: { post: function(){} }
+        };
+        
+        
+        var err;
+        
+        before(function() {
+          getCredential = sinon.stub().yields(null, 'XXXX0x0Xxxx0xXX0xxXX0xXxxxxxxXXX');
+          
+          var txn = {
+            deviceAccount: {
+              status: 'confirmed',
+              phoneNumber: 'XXXXXXXX1234',
+              pushNotifications: {
+                enabled: false
+              },
+              availableMethods: ['sms'],
+              methods: ['sms']
+            },
+            featureSwitches: {
+              mfaApp: {
+                enroll: true
+              },
+              mfaSms: {
+                enroll: true
+              }
+            },
+            transactionToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhdXRoMHwwMHh4MDB4MDAwMHgwMHgwMDAweDAwMDAiLCJleHAiOjE0ODE3NTgzMjMsImlzcyI6Imh0dHBzOi8vYWNtZS5ndWFyZGlhbi5hdXRoMC5jb20iLCJhdWQiOiJodHRwczovL2FjbWUuZ3VhcmRpYW4uYXV0aDAuY29tL2FwaSIsImF6cCI6ImJyb3dzZXIiLCJ0eGlkIjoidHhuX1hYeFhYWHgwMHhYMFh4eFh4WFhYMDB4eHhYMFh4MCJ9.aOSBJGPl6Wxn85i2wdYHREdR1u6X3aazAn0mn3zt042ayjp4MUZNE30gmFnZ2u511cfiQj-K5oIqsbLGlxXjxJRh_vZB7mlAhV7rzDD50pfw7NuaiXyMyE9Pd80eRA9K44IoZ5WP1EXVrMhjqvX_kYQ_fxuET7hKsNN-l_OL46c',
+            availableAuthenticationMethods: ['push', 'otp', 'sms'],
+            availableEnrollmentMethods: ['push', 'otp', 'sms']
+          };
+          
+          sinon.stub(mfaClient.httpClient, 'post').yields(null, txn);
+          guardian = sinon.stub().returns(mfaClient);
+        });
+        
+        after(function() {
+          mfaClient.httpClient.post.restore();
+        });
+        
+        before(function(done) {
+          var factory = $require('../../xom/client/browserish', { 'auth0-guardian-js': guardian });
+        
+          var client = factory(getCredential);
+          client.enroll('auth0|00xx00x0000x00x0000x0000', function(_err) {
+            err = _err;
+            done();
+          });
+        });
+        
+        it('should get authorization credential', function() {
+          expect(getCredential).to.have.been.calledOnce;
+          var call = getCredential.getCall(0);
+          expect(call.args[0]).to.equal('auth0|00xx00x0000x00x0000x0000');
+        });
+        
+        it('should construct MFA API client', function() {
+          expect(guardian).to.have.been.calledOnce;
+          var call = guardian.getCall(0);
+          expect(call.args[0]).to.deep.equal({
+            serviceUrl: 'https://hansonhq.guardian.auth0.com',
+            ticket: 'XXXX0x0Xxxx0xXX0xxXX0xXxxxxxxXXX'
+          });
+        });
+        
+        it('should request MFA API to start flow', function() {
+          expect(mfaClient.httpClient.post).to.have.been.calledOnce;
+          var call = mfaClient.httpClient.post.getCall(0);
+          expect(call.args[0]).to.equal('/api/start-flow');
+          expect(call.args[2]).to.equal(null);
+        });
+        
+        it('should yield error', function() {
+          expect(err).to.be.an.instanceOf(Error);
+          expect(err.message).to.equal('You cannot enroll again. You are already enrolled.');
+          expect(err.errorCode).to.equal('already_enrolled');
+        });
+      }); // already enrolled with SMS
+      
     }); // #enroll
     
     describe('#enrollViaSMS', function() {
